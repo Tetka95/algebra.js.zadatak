fdc5fc5c6709178940186d41501083b322aea1ab
Tetka95 <martina.bobek95@gmail.com>
2024-05-22 19:24:10 +0200

    funkcije za random

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function zbroj () {
-    
+var input1 = Number;
+var input2 = Number;
+
+function randomNum1 () {
+    let input1 = document.getElementById("input1");
+    input1.value = Math.floor(Math.random()*1000) + 1;
+}
+function randomNum2 () {
+    let input2 = document.getElementById("input2");
+    input2.value = Math.floor(Math.random()*1000) + 1;
 }
