bbb162b028a77d8b3a96347ea7585ceb47029f6a
Tetka95 <martina.bobek95@gmail.com>
2024-05-22 20:59:20 +0200

    pokušaj nečega

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-var input1 = Number;
-var input2 = Number;
-
 function randomNum1 () {
-    let input1 = document.getElementById("input1");
+    let input1 = document.getElementById("random1");
     input1.value = Math.floor(Math.random()*1000) + 1;
 }
 function randomNum2 () {
     let input2 = document.getElementById("input2");
     input2.value = Math.floor(Math.random()*1000) + 1;
-} ////dodaj random buttone u html
+}
+
+function zbrojiButton() {
+    let inp1 = document.getElementById("input1").value;
+    let inp2 = document.getElementById("input2").value;
+  
+    let result = inp1.value + inp2.value;
+    let getResult = document.getElementById("rezultat");
+    getResult.value = result;
+    result.classList.add("zelena");
+  }
