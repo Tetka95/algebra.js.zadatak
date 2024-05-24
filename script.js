4c3e0e8f48ff0083426463ccd34d4b480b0978a1
Tetka95 <martina.bobek95@gmail.com>
2024-05-24 18:25:31 +0200

    popravljeno

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function randomNum1 () {
     let input1 = document.getElementById("input1");
     input1.value = Math.floor(Math.random()*1000) + 1;
 }
+
 function randomNum2 () {
     let input2 = document.getElementById("input2");
     input2.value = Math.floor(Math.random()*1000) + 1;
@@ -11,8 +12,18 @@ function zbrojiButton() {
     let inp1 = document.querySelector('#input1').value;
     let inp2 = document.querySelector('#input2').value;
   
-    let result = inp1.value + inp2.value;
+    let result = parseInt(inp1) + parseInt(inp2);
     let getResult = document.getElementById("rezultat");
     getResult.value = result;
-    result.classList.add("zelena");
+    //result.classList.add("zelena");
   }
+
+  function rnd () {
+    let input1 = document.getElementById("input1");
+    input1.value = Math.floor(Math.random()*1000) + 1;
+
+    let input2 = document.getElementById("input2");
+    input2.value = Math.floor(Math.random()*1000) + 1;
+
+    zbrojiButton();
+}
