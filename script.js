12dd7d01034ba6b0c64ab33cd78ca3d4f1fb8b84
Tetka95 <martina.bobek95@gmail.com>
2024-05-24 18:11:54 +0200

    popravljeni buttoni, ali problem u zbroju (NaN)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function randomNum1 () {
-    let input1 = document.getElementById("random1");
+    let input1 = document.getElementById("input1");
     input1.value = Math.floor(Math.random()*1000) + 1;
 }
 function randomNum2 () {
@@ -8,8 +8,8 @@ function randomNum2 () {
 }
 
 function zbrojiButton() {
-    let inp1 = document.getElementById("input1").value;
-    let inp2 = document.getElementById("input2").value;
+    let inp1 = document.querySelector('#input1').value;
+    let inp2 = document.querySelector('#input2').value;
   
     let result = inp1.value + inp2.value;
     let getResult = document.getElementById("rezultat");
